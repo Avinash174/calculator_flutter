7a30990dae8466b63e9a2f5ef0664a4984943972
Avinash175 <avinashmagar15@gmail.com>
2022-12-27 22:58:49 +0530

    New Changes Adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,31 +15,67 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<String> buttons = [
+    'C',
+    'DEL',
+    '%',
+    '/',
+    '9',
+    '8',
+    '7',
+    'x',
+    '6',
+    '5',
+    '4',
+    '-',
+    '3',
+    '2',
+    '1',
+    '+',
+    '0',
+    '.',
+    'ANS',
+    '=',
+  ];
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return Scaffold(
-     backgroundColor: Colors.deepOrange[100],
-     body: Column(
-       children: [
-         Expanded(child: Container()),
-         Expanded(flex:2,child: Container(color: Colors.deepOrange,)),
-       ],
-     ),
-   );
+    return Scaffold(
+      backgroundColor: Colors.deepOrange[100],
+      body: Column(
+        children: [
+          Expanded(child: Container()),
+          Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.deepOrange,
+                child: Center(
+                    child: GridView.builder(
+                      itemCount: buttons.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4),
+                        itemBuilder: (BuildContext context,int index){
+                          return MyButton(buttonText: buttons[index],
+                            color: Colors.deepOrange,
+                            textColor: Colors.white,
+                          );
+
+                        })),
+              )),
+        ],
+      ),
+    );
   }
 }
